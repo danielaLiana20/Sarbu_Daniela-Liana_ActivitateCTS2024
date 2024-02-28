83b37371a1b980df4b6da8b0437426ed3cb12fa2
Daniela <sarbudaniela21@stud.ase.ro>
2024-02-28 08:48:46 +0200

    Seminar2

---
+++ Seminar2/src/model/readere/ElevReader.java
@@ -0,0 +1,32 @@
+package model.readere;
+
+import model.clase.Aplicant;
+import model.clase.Elev;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class ElevReader extends AplicantReader{
+    @Override
+    public List<Aplicant> readAplicant(String file) throws FileNotFoundException {
+        Scanner input2 = new Scanner(new File(file));
+        input2.useDelimiter(",|\n");
+        List<Aplicant> elevi = new ArrayList<Aplicant>();
+
+        while (input2.hasNext()) {
+            Elev elev=new Elev();
+            super.citireAplicant(input2,elev);
+            int clasa = input2.nextInt();
+            elev.setClasa(clasa);
+            String tutore = input2.next();
+            elev.setTutore(tutore);
+            elevi.add(elev);
+        }
+
+        input2.close();
+        return elevi;
+    }
+}
